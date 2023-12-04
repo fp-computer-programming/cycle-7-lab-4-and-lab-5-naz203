fc91091c7eb60c0704f1801d4b54c2a22ea797c4
naz <p24nthompson@fairfieldprep.org>
2023-12-04 11:50:56 -0500

    all done

--- lab7-4/7-4.py
+++ lab7-4/7-4.py
@@ -22,6 +22,33 @@ Ensure your code runs accurately
 
 
 """
+#ryan rinvil (author)
+def find_sum(num1,num2):
+    """
+
+    this functaion takes two numbers as input and returns their sum.
+    """
+    num_sum = num1 + num2
+    return num_sum 
+
+my_sum = find_sum(-10,-5)
+print(my_sum)
+"when the program is run, it will calculate the sum of 111 and 222, store it in my_sum, and then print the valm"
+
+#test case 1
+result_1 = find_sum(5,7)
+#test case 2 
+result_2 = find_sum(-3,8)
+#test case 3
+result_3 = find_sum(0,0)
+#test case 4
+result_4 = find_sum(-10,-5)
+print("all test cases passed successfully")
+
+
+
+#lab7-5
+
 num1 = int
 num2 = int
 num3 = int
@@ -31,7 +58,7 @@ def double_input_values ():
     num2 = int(input ("Enter number 2: "))
     num3 = int(input ("Enter number 3: "))
     num_list.append(num1)
-    num_list.append(num2)
+    num_list.append(num2)       
     num_list.append(num3)
     return num_list
                  
